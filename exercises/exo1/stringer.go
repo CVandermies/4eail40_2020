db7289cb7a632d7c0c847592b088fa0c438b0b2f
CVandermies <charlesvdm@outlook.com>
2020-10-16 04:19:29 +0200

    exo 1 + exo 2

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Implement types Rectangle, Circle and Shape
+
+type Rectangle struct {
+	width, length int
+}
+
+type Circle struct {
+	radius int
+}
+
+type Shape interface {
+	String() string
+}
+
+func (r Rectangle) String() string {
+	return fmt.Sprintf("Rectangle of width %v and length %v", r.width, r.length)
+}
+
+func (c Circle) String() string {
+	return fmt.Sprintf("Circle of radius %v", c.radius)
+}
 
 func main() {
 	r := &Rectangle{2, 3}
--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -10,6 +10,18 @@ type spaceEraser struct {
 	r io.Reader
 }
 
+func (sE spaceEraser) Read(p []byte) (n int, err error) {
+	n, err = sE.r.Read(p)
+	var final []byte
+	for _, caractere := range p {
+		if caractere != 32 { //si le caractère n'est pas un espace
+			final = append(final, caractere)
+		}
+	}
+	copy(p, final)
+	return n, err
+}
+
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
 	r := spaceEraser{s}
